0c1e3883dfde8090eb26f5a40bfd50228a06b6df
andrew20242 <a@a>
2024-04-11 23:43:36 +0300

    crud accounts & endpoints prefix

---
+++ docs/_readme.txt
@@ -0,0 +1,3 @@
+2024-04-11
+Added version prefix for endpoint "/api/v1"
+Added crud for accounts (no data validations)
--- src/main/java/com/reddit/hfs/hfinance/controller/AccountCtrl.java
+++ src/main/java/com/reddit/hfs/hfinance/controller/AccountCtrl.java
@@ -1,14 +1,26 @@
 package com.reddit.hfs.hfinance.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reddit.hfs.hfinance.entity.Account;
 import com.reddit.hfs.hfinance.repository.AccountRepository;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/api/v1")
 public class AccountCtrl {
 
 	private final AccountRepository accountRepository;
@@ -17,10 +29,56 @@ public class AccountCtrl {
 		this.accountRepository = accountRepository;
 	}
 	
+	//Full list of accounts
 	@GetMapping("/accounts")
-	List<Account> all() {
+	public List<Account> all() {
 		return accountRepository.findAll();
 	}
 	
+	//Create account
+	@PostMapping("/accounts")
+	public ResponseEntity<Account> createAccount(
+		@RequestBody Account account
+	) {
+		try {
+			Account accountCreate = accountRepository.save(
+				new Account(account.getName(), account.getType()));
+			return new ResponseEntity<>(accountCreate, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	//Read one account
+	@GetMapping("/accounts/{id}")
+	public Optional<Account> getAccount(
+		@PathVariable Long id
+	) {
+		return accountRepository.findById(id);
+	}
+	
+	//Update one account
+	@PutMapping("/accounts")
+	public ResponseEntity<Account> updateAccount(
+		@RequestBody Account accountNew
+	) {
+		Optional<Account> accountData = accountRepository.findById(accountNew.getId());
+		if (accountData.isPresent()) {
+			Account accountCur = accountData.get();
+			accountCur.setName(accountNew.getName());
+			accountCur.setType(accountNew.getType());
+			return new ResponseEntity<>(accountRepository.save(accountCur), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/accounts/{id}")
+	public ResponseEntity<HttpStatus> deleteAccount(
+		@PathVariable Long id
+	) {
+		accountRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
